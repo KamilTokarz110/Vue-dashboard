22ed321b3aeada8c99f663f47be362819d63781b
Kamil Tokarz <k.tokarz@praktyki.ideo.pl>
2021-08-27 09:19:49 +0200

    Adding styles to Current-Shipment card component

--- src/components/Section_main.vue
+++ src/components/Section_main.vue
@@ -19,8 +19,9 @@
       a.card__button.card__button--filters More Filters
       a.card__button Track
   .card.card--bids
-    img.card--bids__image(src="../assets/bids_logo.svg")
-    h2.card--bids__title Naughty<br>Dog
+    .card--bids__header
+      img.card--bids__image(src="../assets/bids_logo.svg")
+      h2.card--bids__title Naughty<br>Dog
     .card--bids__bottom
       .card--bids__item
         i.fas.fa-lock-open.card--bids__item-icon
@@ -102,21 +103,26 @@
         p.card--todays-shipments__legend-item-text Expedited
   .card.card--current-shipment
     h3.card--current-shipment__title Current Shipment
-    .card--current-shipment__item
-      img.card--current-shipment__image(src="../assets/Nathan_drake.svg")
-      h3.card--current-shipment-title Customer <br> <strong>Nathan Drake</strong>
-    .card--current-shipment__item
-      img.card--current-shipment__image(src="../assets/Not_included.svg")
-      h3.card--current-shipment-title Include cost of fuel in your quote <br> <strong>Not Included</strong>
-    .card--current-shipment__item
-      img.card--current-shipment__image(src="../assets/Cash.svg")
-      h3.card--current-shipment-title Payment Terms <br> <strong>Cash</strong>
-    .card--current-shipment__item
-      img.card--current-shipment__image(src="../assets/Frazer.svg")
-      h3.card--current-shipment-title Cash <br> <strong>Chole Frazer Package</strong>
-    .card--current-shipment__item
-      img.card--current-shipment__image(src="../assets/files.svg")
-      a.card--current-shipment__link Upload documents
+    .card--current-shipment__items
+      .columnn
+        .row
+          .card--current-shipment__item.card--current-shipment__item--1
+            img.card--current-shipment__image(src="../assets/Nathan_drake.svg")
+            h3.card--current-shipment-title Customer <br> <strong>Nathan Drake</strong>
+          .card--current-shipment__item.card--current-shipment__item--2
+            img.card--current-shipment__image(src="../assets/Not_included.svg")
+            h3.card--current-shipment-title Include cost of fuel in your quote <br> <strong>Not Included</strong>
+        .row 
+          .card--current-shipment__item.card--current-shipment__item--3
+            img.card--current-shipment__image(src="../assets/Cash.svg")
+            h3.card--current-shipment-title Payment Terms <br> <strong>Cash</strong>
+          .card--current-shipment__item.card--current-shipment__item--4
+            img.card--current-shipment__image(src="../assets/Frazer.svg")
+            h3.card--current-shipment-title Cash <br> <strong>Chole Frazer Package</strong>
+
+      .card--current-shipment__item-upload
+        img.card--current-shipment__image(src="../assets/files.svg")
+        a.card--current-shipment__link Upload documents
   .card.card--cargo-shipment cargo-shipment
 </template>
 
