7292019f1ec013c2d6f585d3f0dc9c9c6a1a1378
Kamil Tokarz <k.tokarz@praktyki.ideo.pl>
2021-08-30 15:09:05 +0200

    styling shipment-cargo component

--- src/components/Section_main.vue
+++ src/components/Section_main.vue
@@ -124,7 +124,23 @@
       .card--current-shipment__item-upload
         img.card--current-shipment__image(src="../assets/files.svg")
         a.card--current-shipment__link Upload documents
-  //- .card.card--cargo-shipment cargo-shipment
+  .card.card--cargo-shipment
+    h2.card--cargo-shipment__title Shipment cargo
+    ul.card--cargo-shipment__list.card--cargo-shipment__list--title
+      li.card--cargo-shipment__item.card--cargo-shipment__item--bold(
+        v-for="(cargo, index) in cargos",
+        :key="index"
+      ) {{ cargo }}
+    ul.card--cargo-shipment__list.card--cargo-shipment__list--info
+      li.card--cargo-shipment__item-info(
+        v-for="(information, index) in informations",
+        :key="index"
+      ) {{ information }}
+    ul.card--cargo-shipment__list.card--cargo-shipment__list--info
+      li.card--cargo-shipment__item-info(
+        v-for="(information, index) in informations",
+        :key="index"
+      ) {{ information }}
 </template>
 
 <script>
@@ -150,6 +166,24 @@ export default {
         "3 PM",
         "4 PM",
         "5 PM",
+      ],
+      cargos: [
+        "SL No",
+        "Shipper",
+        "Ready Time",
+        "Destination",
+        "Consignee",
+        "Need Time",
+        "Distance",
+      ],
+      informations: [
+        "50265",
+        "Fireflies",
+        "12:00 (IST)",
+        "12:00 (IST)",
+        "31224 Port",
+        "15:00 (IST)",
+        "---",
       ],
     };
   },
